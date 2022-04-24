912219e123d148b8b53e64ee975279f499f09dc6
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-04-24 20:47:01 +0000

    removed unnecessary icons and empty files.

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './components/map';
+import Map from './components/Map';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Map />, rootElement);
