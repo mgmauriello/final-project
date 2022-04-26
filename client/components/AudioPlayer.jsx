0f8cd9912cf2b6fcffc6fe8fe5887788d216d3b4
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-04-26 16:53:30 +0000

    added sounds

--- client/components/AudioPlayer.jsx
+++ client/components/AudioPlayer.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { PlayBtnFill, PauseBtnFill } from 'react-bootstrap-icons';
+
 export default function AudioPlayer(props) {
 
   const [isPlaying, setIsPlaying] = useState(false);
